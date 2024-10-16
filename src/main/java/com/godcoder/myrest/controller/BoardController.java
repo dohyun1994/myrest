0ed478cb0056defee186a1dca832a8b8f546948f
dohyun1994 <hiyaya94@gmail.com>
2024-10-16 17:05:03 +0900

    게시글 작성 Validation 추가

--- src/main/java/com/godcoder/myrest/controller/BoardController.java
+++ src/main/java/com/godcoder/myrest/controller/BoardController.java
@@ -2,11 +2,14 @@ package com.godcoder.myrest.controller;
 
 import com.godcoder.myrest.model.Board;
 import com.godcoder.myrest.repository.BoardRepository;
+import com.godcoder.myrest.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -15,6 +18,9 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardValidator boardValidator;
 
     @GetMapping("/list")
     public String list(Model model) {
@@ -37,7 +43,13 @@ public class BoardController {
     }
 
     @PostMapping("/form")
-    public String greetingSubmit(@ModelAttribute Board board) {
+    public String boardSubmit(@Valid Board board, BindingResult bindingResult) {
+
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "/board/form";
+        }
+
         boardRepository.save(board);
         return "redirect:/board/list";
 
--- src/main/java/com/godcoder/myrest/model/Board.java
+++ src/main/java/com/godcoder/myrest/model/Board.java
@@ -1,11 +1,15 @@
 package com.godcoder.myrest.model;
 
+
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 @Entity
 @Data
@@ -13,7 +17,13 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다.")
     private String title;
+
+    @NotNull
+    @Size(min=2, max=500)
     private String content;
 
 }
