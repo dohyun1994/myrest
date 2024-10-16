c66064402a5ee51db7df29859c32959e474e4a61
dohyun1994 <hiyaya94@gmail.com>
2024-10-16 20:53:25 +0900

    게시글 페이징 적용

--- src/main/java/com/godcoder/myrest/controller/BoardController.java
+++ src/main/java/com/godcoder/myrest/controller/BoardController.java
@@ -4,6 +4,9 @@ import com.godcoder.myrest.model.Board;
 import com.godcoder.myrest.repository.BoardRepository;
 import com.godcoder.myrest.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,8 +26,12 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 10) Pageable pageable) {
+        Page<Board> boards = boardRepository.findAll(pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() +4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
