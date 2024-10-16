1c6c659dba7359784674034ae97b408c4adb3198
dohyun1994 <hiyaya94@gmail.com>
2024-10-16 21:27:17 +0900

    게시글 페이징, 검색기능 적용...

--- src/main/java/com/godcoder/myrest/controller/BoardController.java
+++ src/main/java/com/godcoder/myrest/controller/BoardController.java
@@ -26,8 +26,10 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model, @PageableDefault(size = 10) Pageable pageable) {
-        Page<Board> boards = boardRepository.findAll(pageable);
+    public String list(Model model, @PageableDefault(size = 10) Pageable pageable,
+                      @RequestParam(required = false, defaultValue = "") String searchText) {
+//        Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
         int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
         int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() +4);
         model.addAttribute("startPage", startPage);
